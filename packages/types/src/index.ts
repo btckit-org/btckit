c08bc3e8986eae3b1c277ea05ab09f2c7949983e
kyranjamie <kyran.burraston@gmail.com>
2023-03-16 16:35:49 +0100

    fix: update exports

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,15 +1,14 @@
 import { ValueOf } from './utils';
 
 import './rpc';
-import { DefineGetInfoMethod, GetInfoRequest, GetInfoResponse } from './methods/get-info';
-import { DefineSignPsbtMethod, SignPsbtRequest } from './methods/sign-psbt';
-import {
-  DefineRequestAccountsMethod,
-  RequestAccountsRequest,
-  RequestAccountsResponse,
-} from './methods/request-accounts';
+import { DefineGetInfoMethod } from './methods/get-info';
+import { DefineSignPsbtMethod } from './methods/sign-psbt';
+import { DefineRequestAccountsMethod } from './methods/request-accounts';
 
 export * from './rpc';
+export * from './methods/get-info';
+export * from './methods/sign-psbt';
+export * from './methods/request-accounts';
 
 export type BtcKitMethodMap = DefineGetInfoMethod &
   DefineRequestAccountsMethod &
@@ -21,23 +20,15 @@ export type BtcKitResponses = ValueOf<BtcKitMethodMap>['response'];
 
 export type BtcKitMethodNames = keyof BtcKitMethodMap;
 
-type BtcKitRequest = {
+export type BtcKitRequestFn = {
   <T extends BtcKitMethodNames>(arg: T): Promise<BtcKitMethodMap[T]['response']>;
 };
 
 declare global {
   interface Window {
     btc?: {
-      request: BtcKitRequest;
+      request: BtcKitRequestFn;
       listen(event: string): Promise<any>;
     };
   }
 }
-
-export {
-  GetInfoRequest,
-  GetInfoResponse,
-  SignPsbtRequest,
-  RequestAccountsRequest,
-  RequestAccountsResponse,
-};
--- packages/types/src/methods/request-accounts.ts
+++ packages/types/src/methods/request-accounts.ts
@@ -1,13 +1,15 @@
 import { DefineRpcMethod, RpcRequest, RpcResponse } from '../rpc';
 
-type PaymentTypes = 'p2wpkh' | 'p2wpkh-p2sh' | 'p2tr';
+export type PaymentTypes = 'p2wpkh' | 'p2wpkh-p2sh' | 'p2tr';
 
-interface AddressResponseBody {
-  addresses: {
-    type: PaymentTypes;
-    address: string;
-    derivationPath: string;
-  }[];
+export interface BtcAddress {
+  type: PaymentTypes;
+  address: string;
+  derivationPath: string;
+}
+
+export interface AddressResponseBody {
+  addresses: BtcAddress[];
 }
 
 export interface Params {

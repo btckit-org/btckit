b03e4d07a6855d256eb65599821df4d3f8d44111
kyranjamie <kyran.burraston@gmail.com>
2023-03-22 13:03:53 +0100

    chore: add listen method types

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -19,14 +19,32 @@ export type BtcKitResponses = ValueOf<BtcKitMethodMap>['response'];
 export type BtcKitMethodNames = keyof BtcKitMethodMap;
 
 export interface BtcKitRequestFn {
-  <T extends BtcKitMethodNames>(arg: T): Promise<BtcKitMethodMap[T]['response']>;
+  <T extends BtcKitMethodNames>(arg: T, params?: object | string[]): Promise<
+    BtcKitMethodMap[T]['response']
+  >;
+}
+
+export interface BtcKitListenFn {
+  (method: string, callback: () => void): () => void;
 }
 
 declare global {
   interface Window {
+    /**
+     * Object with method used to interact with BtcKit-compatible wallets. See
+     * for more info https://btckit.org
+     */
     btc?: {
+      /**
+       * Request method. Takes a method name, and optional parameters
+       * @returns Typed response for corresponding method
+       */
       request: BtcKitRequestFn;
-      listen(event: string): Promise<any>;
+      /**
+       * Listen method. Takes an event name to listen for, and a callback function.
+       * @returns An unsubscribe function
+       */
+      listen: BtcKitListenFn;
     };
   }
 }

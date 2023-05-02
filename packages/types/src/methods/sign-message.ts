0bf45178addd6238538b9d7ef4a3bda961a0e560
kyranjamie <kyran.burraston@gmail.com>
2023-05-02 19:46:41 +0200

    fix: accept payment type

--- packages/types/src/methods/sign-message.ts
+++ packages/types/src/methods/sign-message.ts
@@ -1,19 +1,28 @@
 import { DefineRpcMethod, RpcRequest, RpcResponse } from '../rpc';
+import { PaymentTypes } from './get-addresses';
 
 // Implements BIP-322
 // https://github.com/bitcoin/bips/blob/master/bip-0322.mediawiki
-type Bip322MessageTypes = 'legacy' | 'simple' | 'full' | 'proof-of-funds';
+type Bip322MessageTypes = 'legacy' | 'bip322';
 
 export interface SignMessageResponseParams {
-  type: Bip322MessageTypes;
+  type?: Bip322MessageTypes;
   message: string;
+  paymentType: PaymentTypes;
 }
 
 export interface SignMessageResponseBody {
+  [key: string]: unknown;
+
   /**
    * Base64 encoded signature
    */
   signature: string;
+
+  /**
+   * Address that signed the message
+   */
+  address: string;
 }
 
 export type SignMessageRequest = RpcRequest<'signMessage', SignMessageResponseParams>;
